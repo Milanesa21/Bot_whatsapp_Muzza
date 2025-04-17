940ccbbcfbb9282654153ab2515cc8b753aff640
Milanesa21 <diegojara88574@gmail.com>
2025-04-16 22:02:48 -0300

    prueba de cors

--- app.js
+++ app.js
@@ -71,7 +71,12 @@ console.log("Flujos cargados:", flujos);
 const adapterFlow = createFlow(flujos);
 
 // Configuración Express y Socket.io
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use("/pedidos", pedidosRoutes);
 const server = http.createServer(app);
